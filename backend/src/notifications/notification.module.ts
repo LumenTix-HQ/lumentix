71665096f66e6f7479250e4f3193cd9efaa0f6a5
Muhammadcodes112 <funuyallen@gmail.com>
2026-08-27 12:24:20 -0700

    Implement push notification preferences and channel routing (#973)

--- backend/src/notifications/notification.module.ts
+++ backend/src/notifications/notification.module.ts
@@ -2,10 +2,13 @@ import { Module, forwardRef } from '@nestjs/common';
 import { BullModule } from '@nestjs/bull';
 import { NotificationService } from './notification.service';
 import { NotificationProcessor } from './notification.processor';
+import { NotificationPreferencesService } from './notification-preferences.service';
+import { NotificationPreferencesController } from './notification-preferences.controller';
 import { MailerModule } from '../mailer/mailer.module';
 import { UsersModule } from '../users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TicketEntity } from '../tickets/entities/ticket.entity';
+import { User } from '../users/entities/user.entity';
 import { CalendarModule } from '../calendar/calendar.module';
 
 @Module({
@@ -13,12 +16,13 @@ import { CalendarModule } from '../calendar/calendar.module';
     BullModule.registerQueue({
       name: 'notifications', // Must match the string in @Processor
     }),
-    TypeOrmModule.forFeature([TicketEntity]),
+    TypeOrmModule.forFeature([TicketEntity, User]),
     MailerModule,
     forwardRef(() => UsersModule),
     CalendarModule,
   ],
-  providers: [NotificationService, NotificationProcessor],
-  exports: [NotificationService], // Allow Payments/Sponsors to import this
+  controllers: [NotificationPreferencesController],
+  providers: [NotificationService, NotificationProcessor, NotificationPreferencesService],
+  exports: [NotificationService, NotificationPreferencesService], // Allow Payments/Sponsors to import this
 })
 export class NotificationModule { }
